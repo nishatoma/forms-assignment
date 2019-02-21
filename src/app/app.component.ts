a90849d00b6a6021733a396d9b3d5934142babca
nishatoma <nishatoma@gmail.com>
2019-02-20 17:17:02 -0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('myForm') form: NgForm;
+  defaultSubscription = 'basic';
+  submitted: boolean = false;
+
+  user = {
+    email: '',
+    password: '',
+    option: '',
+  };
+
+  submitForm() {
+    this.user.email = this.form.value.email;
+    this.user.password = this.form.value.password;
+    this.user.option = this.form.value.subscriptions;
+    console.log(this.form);
+    this.submitted = true;
+    //this.form.reset();
+  }
 }
